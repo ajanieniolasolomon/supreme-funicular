158d125f5c641b14a248847b5ac0904c0a21940d
ajanieniolasolomon <eniolsolomon@gmail.com>
2025-06-30 04:27:57 -0700

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  nowPaymentsApiKey: 'TB1JMZM-XA8M2QJ-G097CQ0-AZAR0MW' // Add your API key here
+  apiUrl: 'https://casion-xdao.vercel.app',
+nowPaymentsApiKey: 'TB1JMZM-XA8M2QJ-G097CQ0-AZAR0MW'
 };
